74953222f66355d81e738866a0e5001736a22baf
memoglez3 <if722020@iteso.mx>
2023-04-23 16:51:32 -0600

    Update visualizations.py
    Gráficos

--- visualizations.py
+++ visualizations.py
@@ -1,2 +1,37 @@
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
+
+def graficos_plotly(ob, ticker):
+    
+    figs = []
+    
+    for exchange_id in ob.keys():
+        fig = make_subplots(specs=[[{"secondary_y": False}]])
+        
+        fig.add_trace(go.Scatter(x = ob[exchange_id].index, y = ob[exchange_id]["Level"], name = exchange_id), 
+                       secondary_y = False,)
+        fig.update_layout(title = "Level: " + ticker,  yaxis_title = "Niveles", xaxis_title = "Fecha")
+        
+        fig.add_trace(go.Scatter(x = ob[exchange_id].index, y = ob[exchange_id]["Ask Volume"], name = exchange_id), 
+                       secondary_y = False,)
+        fig.update_layout(title = "Ask Volume: " + ticker,  yaxis_title = "Volumen", xaxis_title = "Fecha")
+        
+        fig.add_trace(go.Scatter(x = ob[exchange_id].index, y = ob[exchange_id]["Bid Volume"], name = exchange_id), 
+                       secondary_y = False,)
+        fig.update_layout(title = "Bid Volume: " + ticker,  yaxis_title = "Volumen", xaxis_title = "Fecha")
+        
+        fig.add_trace(go.Scatter(x = ob[exchange_id].index, y = ob[exchange_id]["Total Volume"], name = exchange_id), 
+                       secondary_y = False,)
+        fig.update_layout(title = "Total Volume: " + ticker,  yaxis_title = "Volumen", xaxis_title = "Fecha")
+        
+        fig.add_trace(go.Scatter(x = ob[exchange_id].index, y = ob[exchange_id]["Mid Price"], name = exchange_id), 
+                       secondary_y = False,)
+        fig.update_layout(title = "Mid Price: " + ticker,  yaxis_title = "Precio Dólares", xaxis_title = "Fecha")
+        
+        fig.add_trace(go.Scatter(x = ob[exchange_id].index, y = ob[exchange_id]["VWAP"], name = exchange_id), 
+                       secondary_y = False,)
+        fig.update_layout(title = "VWAP: " + ticker,  yaxis_title = "Precio Dólares", xaxis_title = "Fecha")
+        
+        figs.append(fig)
+    
+    return figs
